4c7dbe689e4a09c9ac21b5550d061f13cb10364a
NikolayDvoinikh <nikolaydvoinikh@gmail.com>
2024-01-22 16:03:33 +0200

    routing

---
+++ src/app/(admin)/layout.tsx
@@ -0,0 +1,28 @@
+import Sidebar from '../components/sidebar';
+
+export interface LayoutProps {
+  children: React.ReactNode;
+  header: React.ReactNode;
+  toolbar: React.ReactNode;
+  modal: React.ReactNode;
+}
+
+export default function Layout({
+  children,
+  header,
+  toolbar,
+  modal,
+}: LayoutProps) {
+  return (
+    <>
+      <Sidebar />
+      <div className="ml-60">{children}</div>
+      {/* {modal}
+      {header}
+      <main>
+        {toolbar}
+        {children}
+      </main> */}
+    </>
+  );
+}
